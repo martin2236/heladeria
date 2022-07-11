1a7999178b7d0bb74ed73db09e63e743a2a44d80
Jorgeeperalta <edudela36@gmail.com>
2022-07-11 10:56:44 -0300

    Categorias y Listado

--- src/router/index.js
+++ src/router/index.js
@@ -1,6 +1,8 @@
 import Vue from 'vue'
 import VueRouter from 'vue-router'
+import Listado from '../views/Listado.vue'
 import Home from '../views/Home.vue'
+import Categorias from '../views/Categorias.vue'
 
 Vue.use(VueRouter)
 
@@ -11,12 +13,14 @@ const routes = [
     component: Home
   },
   {
-    path: '/about',
-    name: 'About',
-    // route level code-splitting
-    // this generates a separate chunk (about.[hash].js) for this route
-    // which is lazy-loaded when the route is visited.
-    component: () => import(/* webpackChunkName: "about" */ '../views/About.vue')
+    path: '/listado',
+    name: 'Listado',
+    component: Listado
+  },
+  {
+    path: '/categorias',
+    name: 'Categorias',
+    component: Categorias
   }
 ]
 
