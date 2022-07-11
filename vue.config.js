d34a6914b9f3350bd93da294f6b8ef9d239b54c2
Jorgeeperalta <edudela36@gmail.com>
2022-07-11 11:37:40 -0300

    vue.config and swal

--- vue.config.js
+++ vue.config.js
@@ -1,5 +1,17 @@
 module.exports = {
   transpileDependencies: [
     'vuetify'
-  ]
+  ],
+ 
+    pwa: {
+      name: 'My App',
+      themeColor: '#4DBA87',
+      msTileColor: '#000000',
+      workboxPluginMode: 'GenerateSW',
+      worboxOptions: {
+        skipWaiting: true
+      }
+  
+    }
+  
 }
