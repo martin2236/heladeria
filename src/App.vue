61c560afcbb544542530797d01f5e70ab0f9a9d9
martin2236 <martoxxx100@gmail.com>
2022-07-12 21:07:25 -0300

    todo

--- src/App.vue
+++ src/App.vue
@@ -1,32 +1,131 @@
 <template>
-  <div id="app">
-    <div id="nav">
-      <router-link to="/">Home</router-link> |
-      <router-link to="/about">About</router-link>
-    </div>
-    <router-view/>
-  </div>
+  <v-app>
+    <v-app-bar
+      app
+      color="primary"
+      dark
+      fixed
+      
+    >
+    <v-col
+       cols="2"
+       lg="4"
+       md="4"
+       sm="4"
+        class="d-flex align-center"
+    >
+         <v-app-bar-nav-icon @click="drawer = true"></v-app-bar-nav-icon>
+        <v-toolbar-title class="titulo">Helados ya!</v-toolbar-title>
+    </v-col>
+    <v-col
+       cols="8"
+       lg="4"
+       md="4"
+       sm="4"
+    >
+         <v-text-field 
+        class="ml-lg-5 mt-7 pa-0 buscar"
+        style="align-self:center"
+        placeholder="buscar heladeria"
+        append-icon="mdi-magnify"
+        @click:append="toggleMarker"
+        filled
+        rounded
+        dense
+    ></v-text-field>
+    </v-col>
+    <v-col
+    cols="2"
+    lg="4"
+    md="4"
+    sm="4"
+       class="d-flex justify-end"
+    >
+        <v-btn
+      class="mr-n3 mx-lg-2"
+      fab
+      dark
+      small
+      color="indigo"
+    >
+      <v-icon dark>
+        mdi-cart
+      </v-icon>
+    </v-btn>
+    </v-col>
+    
+    <!--<v-btn text class="ubicacion"><v-icon class="ubicacion-icon">mdi-map-marker</v-icon> Ingresar mi ubicación</v-btn>-->
+   
+   
+    </v-app-bar>
+    <v-navigation-drawer
+      v-model="drawer"
+      absolute
+      temporary
+      fixed
+    >
+      <v-list
+        nav
+        dense
+      >
+        <v-list-item-group
+          v-model="group"
+          active-class="deep-purple--text text--accent-4"
+        >
+          <v-list-item>
+            <v-list-item-icon>
+              <v-icon>mdi-home</v-icon>
+            </v-list-item-icon>
+            <v-list-item-title>Inicio</v-list-item-title>
+          </v-list-item>
+
+          <v-list-item>
+            <v-list-item-icon>
+              <v-icon>mdi-account</v-icon>
+            </v-list-item-icon>
+            <v-list-item-title>Iniciar sesion</v-list-item-title>
+          </v-list-item>
+        </v-list-item-group>
+      </v-list>
+    </v-navigation-drawer>
+
+    <v-main>
+      <router-view/>
+    </v-main>
+  </v-app>
 </template>
 
-<style>
-#app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
+<script>
+
+export default {
+  name: 'App',
+
+  data: () => ({
+      drawer: false,
+      group: null,
+       marker: true,
+    }),
+    methods:{
+         toggleMarker () {
+        this.marker = !this.marker
+      },
+    }
+};
+</script>
+ <style lang="css" >
+ #app{
+     background-color: aliceblue;
+ }
+    
+.ubicacion{
+    font-size: 15px;
 }
 
-#nav {
-  padding: 30px;
-}
-
-#nav a {
-  font-weight: bold;
-  color: #2c3e50;
-}
-
-#nav a.router-link-exact-active {
-  color: #42b983;
-}
+ 
+    @media (max-width: 376px) { 
+        .titulo{
+            display: none;
+        }
+     }
+    
 </style>
--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,18 +1,430 @@
 <template>
-  <div class="home">
-    <img alt="Vue logo" src="../assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
-  </div>
+ <div>
+     <v-card 
+     width="100%" 
+     color="primary" 
+     class="mt-5 d-flex flex-column align-center justify-center">
+         <v-card-title class="mt-8 text-center" style="font-size:30px; color:#ffffff; text-align:center;">
+            <p> No esperes más pedí tus <span style="font-size:40px; font-weight:bold; margin-left:7px;"> Helados ya!</span></p>
+        </v-card-title>
+         <v-text-field
+            class="mt-8"
+            prepend-inner-icon="mdi-map-marker"
+            placeholder="seleccione una ubicacion"
+            background-color="white"	
+            outlined
+         ></v-text-field>
+           <v-card-text class="d-lg-flex d-md-flex d-sm-flex flex-lg-row mb-10" >
+                <v-col
+                class="text-center"
+             cols="12"
+             lg="7"
+             md="6"
+             sm="6"
+             >
+                 <v-btn
+                 class="ml-lg-15 mr-lg-n16"
+                 color="white"
+                    text
+                    small
+                 >
+                     <v-icon small class="mr-2">mdi-map-marker-down</v-icon>
+                     Usar ubicación actual
+                 </v-btn>
+             </v-col>
+             <v-col
+             class="text-center text-lg-start text-md-start text-sm-start pl-0"
+            cols="12"
+            lg="4"
+            md="6"
+            sm="6"
+             >
+                <v-btn
+                class="px-0 text-center ml-lg-n16"
+                 color="white"
+                    text
+                    small
+                 >
+                     Iniciar sesion 
+                 </v-btn>
+                 <span style="font-size:14px; color:#fff;"> y ver ubicaciones previas</span>
+             </v-col>
+           </v-card-text>
+     </v-card>
+     <v-card class="mt-5">
+         <v-card-title>
+             Que estás buscando hoy?
+         </v-card-title>
+         <v-card-text class="d-lg-flex d-md-flex d-sm-flex flex-lg-row flex-sm-column">
+          <v-col
+            cols="12"
+            lg="6"
+            md="6"
+            sm="12"
+          >
+              <v-hover v-slot="{ hover }">
+                <v-card
+                class="mx-auto"
+                color="grey lighten-4"
+                max-width="600"
+                >
+                <v-img
+                    src="../assets/helados.jpg"
+                    class="imagen-filtro"
+                >
+                    <v-expand-transition>
+                    <div
+                        v-if="hover"
+                        class="d-flex flex-column transition-fast-in-fast-out black darken-2 v-card--reveal  white--text"
+                        style="height: 100%;"
+                    >
+                        <p style="font-size:20px; text-align:center; font-weight:bold;">
+                            Encontra las mejores heladerias del partido de la costa
+                        </p>
+                        <router-link to="" style="text-decoration:underline; font-size:20px;">ver más</router-link>
+                    </div>
+                    </v-expand-transition>
+                </v-img>
+                </v-card>
+            </v-hover>
+          </v-col>
+          <v-col
+          class="mb-5"
+            cols="12"
+            lg="6"
+            md="6"
+            sm="12"
+          >
+              <v-hover v-slot="{ hover }">
+                <v-card
+                class="mx-auto"
+                color="grey lighten-4"
+                max-width="600"
+                >
+                <v-img
+                    src="../assets/helados-artesanales.jpg"
+                    class="imagen-filtro"
+                >
+                    <v-expand-transition>
+                    <div
+                        v-if="hover"
+                        class="d-flex flex-column transition-fast-in-fast-out black darken-2 v-card--reveal  white--text"
+                        style="height: 100%;"
+                    >
+                        <p style="font-size:20px; text-align:center; font-weight:bold; opacity:1;">
+                            Encontra las mejores heladerias artesanales del partido de la costa
+                        </p>
+                        <router-link to="" style="text-decoration:underline; font-size:20px;">ver más</router-link>
+                    </div>
+                    </v-expand-transition>
+                </v-img>
+                </v-card>
+            </v-hover>
+          </v-col>
+         </v-card-text>
+     </v-card >
+     <v-card class="mt-5" >
+         <v-card-title class=" text-center">Tu postre fácil y rápido</v-card-title>
+         <v-card-title class="ml-lg-15 mb-15 titulo-2" >
+             Aprovechá mejor tu tiempo y disfrutá de tus 
+             <span class="logo-centro" style="margin-left:7px;color:#1976D2; font-wight:900; font-size:60px;">Helados ya!</span>
+             </v-card-title>
+         <v-card-text class="d-lg-flex d-md-flex d-sm-flex flex-lg-row mt-5">
+             <v-col class="text-center">
+                    <v-img
+                    class="mx-auto rounded-circle"
+                    src="../assets/easy.png"
+                    height="100"
+                    width="100"
+                    ></v-img> 
+              
+                    <h4 class="mt-5">titulo</h4>
+                    <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Maxime esse itaque sequi ullam velit! Voluptates aliquam dolor quo eaque quas!</p>
+             </v-col>
+               <v-col class="text-center">
+                    <v-img
+                    class="mx-auto rounded-circle"
+                    src="../assets/fast.png"
+                    height="100"
+                    width="100"
+                    ></v-img> 
+              
+                    <h4 class="mt-5">titulo</h4>
+                    <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Maxime esse itaque sequi ullam velit! Voluptates aliquam dolor quo eaque quas!</p>
+             </v-col>
+               <v-col class="text-center mb-15">
+                    <v-img
+                    class="mx-auto rounded-circle"
+                    src="../assets/ofertas.png"
+                    height="100"
+                    width="100"
+                    ></v-img> 
+              
+                    <h4 class="mt-5">titulo</h4>
+                    <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Maxime esse itaque sequi ullam velit! Voluptates aliquam dolor quo eaque quas!</p>
+             </v-col>
+         </v-card-text>
+     </v-card>
+     <v-card class="mt-5">
+         <v-col class="d-flex flex-row justify-space-between align-center">
+            <v-card-title class="ml-n3">
+                Heladerias mas visitadas
+            </v-card-title>
+            <v-btn
+            small
+            text
+            class="mx-0 px-0 mr-lg-5"
+            color="primary"
+            >
+                ver más
+            </v-btn>
+         </v-col>
+         <v-card-text>
+             <v-sheet
+                class="mx-0 px-0"
+            >
+                <v-slide-group
+                v-model="model"
+                class="pa-4"
+                active-class="success"
+                >
+                <v-slide-item
+                    v-for="n in 15"
+                    :key="n"
+                    v-slot="{ active, toggle }"
+                >
+                    <v-card
+                    :color="active ? undefined : 'white'"
+                    class="ma-4"
+                    height="300"
+                    width="200"
+                    @click="toggle"
+                    >
+                    <v-img
+                    src="../assets/hela1.jpg"
+                    height="200"
+                    width="200"
+                    ></v-img>
+                    <h4 class="text-center">una heladeria</h4>
+                    <p class="text-center">San Bernardo</p>
+                    <p class="text-center">lun a dom - 9pm a 4am</p>
+                    </v-card>
+                </v-slide-item>
+                </v-slide-group>
+            </v-sheet>
+         </v-card-text>
+     </v-card>
+     <v-card class="mt-5 d-flex flex-column">
+         <v-card-title>
+             Algunos de nuestros clientes
+         </v-card-title>
+         <v-card-text class="mt-10 align-self-center d-flex flex-row flex-wrap justify-space-around logos-centro">
+             <v-col
+            cols="4"
+             >
+                  <v-img
+                    class="mx-auto rounded-circle"
+                    src="../assets/logos/grido.jpg"
+                    height="90"
+                    width="90"
+                    ></v-img> 
+             </v-col>
+             <v-col
+             cols="4"
+             >
+                  <v-img
+                    class="mx-auto rounded-circle"
+                    src="../assets/logos/frahel.jpeg"
+                    height="90"
+                    width="90"
+                    ></v-img> 
+             </v-col>
+             <v-col
+             cols="4"
+             >
+                  <v-img
+                    class="mx-auto rounded-circle"
+                    src="../assets/logos/morato.png"
+                    height="90"
+                    width="90"
+                    ></v-img> 
+             </v-col>
+             <v-col
+                cols="4"
+             >
+                  <v-img
+                    class="mx-auto rounded-circle"
+                    src="../assets/logos/prego.png"
+                    height="100"
+                    width="100"
+                    ></v-img> 
+             </v-col>
+             <v-col
+                cols="4"
+             >
+                  <v-img
+                    class="mx-auto rounded-circle"
+                    src="../assets/logos/sei-tu.jpg"
+                    height="100"
+                    width="100"
+                    ></v-img> 
+             </v-col>
+             <v-col
+                cols="4"
+             >
+                  <v-img
+                    class="mx-auto rounded-circle"
+                    src="../assets/logos/vanshelato.jpg"
+                    height="100"
+                    width="100"
+                    ></v-img> 
+             </v-col>
+         </v-card-text>
+     </v-card>
+     <v-card class="mt-5" color="primary">
+         <v-card-title class="white--text">
+             Que opinan de nosotros?
+         </v-card-title>
+         <v-card-title class="ml-lg-15 mb-10 titulo-2 white--text" >
+             Testimonios de personas que utilizan
+             <span class="logo-centro" style="margin-left:7px;color:#fff; font-wight:900; font-size:60px;">Helados ya!</span>
+             </v-card-title>
+         <v-card-text>
+              <v-sheet
+                class="mx-0 px-0"
+                color="primary"
+                
+            >
+                <v-slide-group
+                v-model="model"
+                class="py-4 px-0"
+                active-class="success"
+                >
+                <v-slide-item
+                    v-for="t in 15"
+                    :key="t"
+                    v-slot="{ active }"
+                >
+                    <v-card
+                    :color="active ? undefined : 'white'"
+                    class="mx-5 mt-10"
+                    height="200"
+                    width="340"
+                    >
+                    <v-img
+                    class="rounded-circle"
+                    style="position:absolute; top:-40px;margin-left: auto; margin-right: auto;left: 0;right: 0;"
+                    src="../assets/persona.jpg"
+                    height="80"
+                    width="80"
+                    ></v-img>
+                    <p class="text-center" style="margin-top:60px">Lorem ipsum dolor sit amet consectetur, adipisicing elit. Quod dolore pariatur nesciunt, repellendus accusantium quibusdam harum quas vitae sit aspernatur?</p>
+                    <h4 class="text-center">Iron Man</h4>
+                    </v-card>
+                </v-slide-item>
+                </v-slide-group>
+            </v-sheet>
+         </v-card-text>
+     </v-card>
+     <v-card class="mt-5">
+         <v-card-title>Querés formar parte?</v-card-title>
+         <v-card-text class="d-lg-flex d-md-flex d-sm-flex flex-lg-row ">
+             <v-col 
+             cols="12"
+             lg="5"
+             md="5"
+             sm="5"
+             >
+            <v-img
+                class="mt-lg-5 ml-lg-15"
+                height="250"
+                width="370"
+                src="../assets/heladeria2.jpg"
+                >
+                </v-img>
+             </v-col>
+             <v-col
+             cols="12"
+             lg="7"
+             md="7"
+             sm="7"
+             class="d-flex flex-column align-center"
+             >
+                <h2 class="text-center mt-5" style="font-size:40px; line-height:40px;">Vos también podés ser miembro de Helados ya!</h2>
+                <h4 class="text-center mt-5">Registra tu heladería y empeza a sumar clientes</h4>
+                <v-btn class="mt-5" color="primary">Quiero ser miembro</v-btn>
+             </v-col>
+         </v-card-text>
+
+     </v-card>
+    </div>
 </template>
 
 <script>
 // @ is an alias to /src
-import HelloWorld from '@/components/HelloWorld.vue'
+
 
 export default {
-  name: 'Home',
-  components: {
-    HelloWorld
-  }
+ data: () => ({
+      model: null,
+ })
 }
 </script>
+<style scoped>
+ .v-text-field{
+      width: 49%;
+}
+.titulo-2{
+    font-size: 40px;
+}
+.imagen-filtro{
+    height: 360px;
+}
+    .logos-centro{
+            max-width: 600px !important;
+            height: 300px;
+        }
+.v-card--reveal {
+  align-items: center;
+  bottom: 0;
+  justify-content: center;
+  opacity: .8;
+  position: absolute;
+  width: 100%;
+}
+@media (max-width: 376px) { 
+        .v-text-field{
+            width: 90%;
+        }
+        
+        .titulo-2{
+            font-size: 25px;
+            text-align: center;
+        }
+        .logo-centro{
+            margin-top: 20px;
+        }
+        .imagen-filtro{
+            height: 240px;
+        }
+     }
+     @media (min-width: 376px) { 
+         .titulo-2{
+            text-align: center;
+        }
+        .logo-centro{
+            margin-top: 20px;
+        }
+        
+    }
+@media (min-width: 1100px) { 
+.v-text-field{
+    width: 50%;
+}
+        .logo-centro{
+            margin-top: 0px;
+        }
+
+}
+
+</style>
